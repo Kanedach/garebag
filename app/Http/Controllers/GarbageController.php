9d42cc78b45c61878516236559ec887890226467
Stutz Fernando, INI-DOS-DCI-S2A <Fernando.Stutz@swisscom.com>
2020-07-05 19:53:31 +0200

    Add Auth

--- app/Http/Controllers/GarbageController.php
+++ app/Http/Controllers/GarbageController.php
@@ -2,45 +2,90 @@
 
 namespace App\Http\Controllers;
 
-use App\Garbage;
+use App\Tenant;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class GarbageController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $data['garbages'] = DB::table('garbages')
-            ->join('tenants', 'garbages.tenant_id', '=', 'tenants.id')
-            ->select('garbages.*', 'tenants.name')
-            ->orderBy('garbages.date', 'desc')
-            ->get();
-        return view('welcome', $data);
+        //
     }
 
-    public function create($id)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        $url = [];
-        $url['id'] = $id;
-        return view('form/create', $url);
+        $data = [];
+        $data['tenants'] = Tenant::all();
+        return view('garbage/create', $data);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'weight' => 'bail | numeric | required',
-            'date' => 'bail | date | required',
-            'tenant' => 'bail | required',
-        ]);
-
-        $garbage = new Garbage;
-        $garbage->weight = $request->input('weight');
-        $garbage->date = $request->input('date');
-        $garbage->tenant_id = $request->input('tenant');
-        $garbage->save();
-
-        return redirect()->route('garbages');
+        //
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
