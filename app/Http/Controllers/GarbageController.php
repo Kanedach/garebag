f08cb8bd8e5e2016b764980474b56062ecf6a0d0
Stutz Fernando, INI-DOS-DCI-S2A <Fernando.Stutz@swisscom.com>
2020-07-16 20:00:53 +0200

    fix

--- app/Http/Controllers/GarbageController.php
+++ app/Http/Controllers/GarbageController.php
@@ -58,6 +58,7 @@ class GarbageController extends Controller
     {
         $data = [];
         $data['garbage'] = Garbage::find($id);
+        $data['tenant'] = Tenant::find($id);
         return view('garbage/show', $data);
     }
 
